ca00aa1a8fabfce0c7ebead74c871371a209fa10
Oncle Tom <thomas@oncle-tom.net>
2013-04-24 14:56:29 +0100

    Digging in stalling/erroring listening (due to 402 redirect in the stream flux)

--- src/lib/fip-radio.js
+++ src/lib/fip-radio.js
@@ -29,10 +29,35 @@ FIPRadio.prototype.bootstrap = function bootstrap(){
  *
  */
 FIPRadio.prototype.configureAudio = function configureAudio(){
+  var self = this;
   var audio = new Audio();
 
   audio.preload = "auto";
   audio.src = this.url;
+
+  audio.addEventListener('error', function(e){
+    console.log("An error occured.")
+    console.dir(e);
+
+    self.stop();
+  });
+
+  audio.addEventListener('stalled', function(e){
+    console.log("An error occured (stall).")
+    console.dir(e);
+
+    self.stop();
+  });
+
+  audio.addEventListener('stalled', FIPRadio.logEvent);
+  audio.addEventListener('waiting', FIPRadio.logEvent);
+  audio.addEventListener('loadeddata', FIPRadio.logEvent);
+  audio.addEventListener('canplay', FIPRadio.logEvent);
+  audio.addEventListener('loadstart', FIPRadio.logEvent);
+  audio.addEventListener('emptied', FIPRadio.logEvent);
+  audio.addEventListener('play', FIPRadio.logEvent);
+  audio.addEventListener('pause', FIPRadio.logEvent);
+
   audio.load();
 
   this.audio = audio;
@@ -54,6 +79,7 @@ FIPRadio.prototype.play = function play(){
     this.configureAudio();
   }
 
+  this.audio.src = this.url;
   this.audio.play();
   this.state = 'playing';   //not really true, should be 'buffering' then async 'playing'
 };
@@ -63,7 +89,12 @@ FIPRadio.prototype.play = function play(){
  */
 FIPRadio.prototype.stop = function stop(){
   this.audio.pause();
+  this.audio.src = '';
   this.state = 'paused';
 };
 
 FIPRadio.prototype.pause = FIPRadio.prototype.stop;
+
+FIPRadio.logEvent = function logEvent(event){
+  console.log("Audio Element State: ", event.type);
+};
