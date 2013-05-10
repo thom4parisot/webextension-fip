67f38f5b35b0aed961f092014c39b7bf631fe675
Oncle Tom <thomas@oncle-tom.net>
2013-05-10 17:12:56 +0100

    Updating page title from the controller

--- src/now-playing/controller.js
+++ src/now-playing/controller.js
@@ -8,13 +8,17 @@
  * @constructor
  */
 function NowPlayingController($scope){
-  $scope.title = chrome.i18n.getMessage('extension_name');
-
-  $scope.broadast = {
+  $scope.broadcast = {
     date: "2012",
     artist: "La Danse qui Pense (cd Promo)",
     album: "Morro",
     title: "L'insomnie",
     cover: "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs"
   };
+
+  // Trick to update the title outside the scope (avoids the controller to be global)
+  $scope.$root.title = chrome.i18n.getMessage('extension_name');
 }
+
+// And now deal with minification!
+NowPlayingController.$inject = ['$scope'];
