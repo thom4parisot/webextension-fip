29bf27aa23e7e493c63e853b7a12fe90c10db829
oncletom <thomas@oncle-tom.net>
2013-05-14 00:38:53 +0100

    JSHint compliance

--- src/now-playing/chrome-service.js
+++ src/now-playing/chrome-service.js
@@ -1,5 +1,11 @@
 "use strict";
 
+/* globals angular, chrome */
+
+/**
+ * Chrome API Abstraction.
+ * Especially used to enables unit/function tests without relying on Chrome API direct access.
+ */
 angular.module('ChromeService', [])
   .factory('translate', function(){
     return chrome.i18n.getMessage.bind(chrome.i18n);
@@ -9,4 +15,4 @@ angular.module('ChromeService', [])
       process: chrome.extension.getBackgroundPage().process,
       addListener: chrome.runtime.onMessage.addListener.bind(chrome.runtime.onMessage)
     };
-  })
+  });
