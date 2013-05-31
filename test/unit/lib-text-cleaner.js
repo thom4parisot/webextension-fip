48084463c83c619d99b40915748895ed62021ebc
Oncle Tom <thomas@oncle-tom.net>
2013-05-31 17:07:58 +0100

    Initiating tests

---
+++ test/unit/lib-text-cleaner.js
@@ -0,0 +1,16 @@
+"use strict";
+
+suite('Text Cleaner', function(){
+
+  test('Cleaning PROMO', function(){
+
+  });
+
+  test('Cleaning incomplete titles', function(){
+
+  });
+
+  test('Cleaning artist names', function(){
+
+  });
+});
