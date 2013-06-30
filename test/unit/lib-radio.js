191676143db5ac2864192a9c90d4a0add613607d
oncletom <thomas@oncle-tom.net>
2013-06-30 16:07:08 +0100

    Added tests for volume

---
+++ test/unit/lib-radio.js
@@ -0,0 +1,28 @@
+"use strict";
+
+suite('Radio', function(){
+  var radio;
+
+  setup(function(){
+    radio = new Radio();
+  });
+
+  test('#volume.get', function(){
+    expect(radio.playbackVolume).to.equal(100);
+    expect(radio.volume()).to.be(radio.playbackVolume);
+  });
+
+  test('#volume.set - normal', function(){
+    radio.volume(50);
+    expect(radio.playbackVolume).to.equal(50);
+    expect(radio.volume()).to.be(50);
+  });
+
+  test('#volume.set - failing', function(){
+    ["null", null, undefined].forEach(function(volumeValue){
+      radio.volume(volumeValue);
+      expect(radio.playbackVolume).to.equal(100);
+      expect(radio.volume()).to.be(100);
+    })
+  });
+});
