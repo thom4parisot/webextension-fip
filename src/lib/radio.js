2bca0aea1ca16d5b6ae306c0ddf5a21e6760f021
Charles Bonnissent <cbonnissent@users.noreply.github.com>
2015-06-24 09:06:45 +0200

    New playback URL
    Update the playback URL after france radio url modif ( see : http://espacepublic.radiofrance.fr/23-juin-2015-inaccessibilit-de-nos-antennes-tunein-et-itunes )

--- src/lib/radio.js
+++ src/lib/radio.js
@@ -13,7 +13,8 @@ var Radio = machina.Fsm.extend({
   "initialState": "stopped",
 
   // Behavior config
-  "playbackUrl": "http://mp3lg.tdf-cdn.com/fip/all/fiphautdebit.mp3",
+  "playbackUrl": "http://audio.scdn.arkena.com/11016/fip-midfi128.mp3",
+  //"playbackUrl": "http://mp3lg.tdf-cdn.com/fip/all/fiphautdebit.mp3",
   //"playbackUrl": "http://mp3.live.tv-radio.com/fip/all/fiphautdebit.mp3",
   "playbackVolume": 100,
 
