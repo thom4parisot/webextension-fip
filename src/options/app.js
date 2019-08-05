efb2dc0455587b2727027f7ac4a5b8826da8b47e
Thomas Parisot <oncletom@users.noreply.github.com>
2019-08-05 22:21:16 +0200

    feat(now-playing): display graphql results

--- src/options/app.js
+++ src/options/app.js
@@ -1,13 +1,12 @@
 import angular from 'angular';
 
 import ChromeService from '../lib/angular/chrome-service.js';
-import TextCleanerFilters from '../lib/angular/text-cleaner-service.js';
 
 import OptionsController from './options-controller.js';
 import ScrobblingController from './scrobbling-controller.js';
 
 
-const App = angular.module('options', ['ChromeService', 'TextCleanerFilters']).run(() => {
+const App = angular.module('options', ['ChromeService']).run(() => {
   document.documentElement.setAttribute('lang', navigator.language);
 });
 
